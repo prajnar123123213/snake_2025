4edc2465dd48eb2e7c085d2b3c826be96a1f6223
jm1021 <jmort1021@gmail.com>
2024-08-29 06:32:41 -0700

    add more to venv script

--- scripts/venv.sh
+++ scripts/venv.sh
@@ -2,6 +2,7 @@
 
 # Verify the installation and check the Python version
 python --version
+bundle --version
 
 # Create a virtual environment if it doesn't exist
 if [ ! -d "venv" ]; then
@@ -10,6 +11,10 @@ fi
 
 # Activate the virtual environment
 source venv/bin/activate
+git config --global user.name jm1021
+git config --global user.email jmort1021@gmail.com
 
 # Install the required Python packages
 pip install -r requirements.txt
+bundle install
+
