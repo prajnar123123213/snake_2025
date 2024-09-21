8c19b63d2ab24355854c80c4f31fdb0ca110823a
jm1021 <jmort1021@gmail.com>
2024-09-20 20:50:17 -0700

    make x an y velocity variables, move listneres

--- assets/js/player/GameControl.js
+++ assets/js/player/GameControl.js
@@ -1,11 +1,6 @@
 import GameEnv from './GameEnv.js';
 import Player from './Player.js';
 
-let keys = {
-    right: { pressed: false },
-    left: { pressed: false }
-};
-
 let player;
 
 const GameControl = {
@@ -23,13 +18,6 @@ const GameControl = {
     gameLoop: function() {
         GameEnv.ctx.clearRect(0, 0, GameEnv.canvas.width, GameEnv.canvas.height);
         player.update(GameEnv.ctx);
-        if (keys.right.pressed && player.position.x + player.width <= GameEnv.canvas.width - 50) {
-            player.velocity.x = 15;
-        } else if (keys.left.pressed && player.position.x >= 50) {
-            player.velocity.x = -15;
-        } else {
-            player.velocity.x = 0;
-        }
         requestAnimationFrame(GameControl.gameLoop);
     },
 
@@ -37,39 +25,5 @@ const GameControl = {
         GameEnv.resize();
     }
 };
-
-addEventListener('keydown', ({ keyCode }) => {
-    switch (keyCode) {
-        case 65: // 'A' key
-            keys.left.pressed = true;
-            break;
-        case 68: // 'D' key
-            keys.right.pressed = true;
-            break;
-        case 87: // 'W' key
-            player.velocity.y -= 20;
-            break;
-        case 83: // 'S' key
-            player.velocity.y += 20;
-            break;
-    }
-});
-
-addEventListener('keyup', ({ keyCode }) => {
-    switch (keyCode) {
-        case 65: // 'A' key
-            keys.left.pressed = false;
-            break;
-        case 68: // 'D' key
-            keys.right.pressed = false;
-            break;
-        case 87: // 'W' key
-            player.velocity.y = 0;
-            break;
-        case 83: // 'S' key
-            player.velocity.y = 0;
-            break;
-    }
-});
 
 export default GameControl;
--- assets/js/player/Player.js
+++ assets/js/player/Player.js
@@ -3,10 +3,20 @@ import GameEnv from './GameEnv.js';
 class Player {
     constructor() {
         // Initialize player properties
-        this.position = { x: GameEnv.innerWidth / 2, y: GameEnv.innerHeight - 50 };
+        this.size = 50;
+        this.xVelocity = 15;
+        this.yVelocity = 10;
+        this.position = { x: GameEnv.innerWidth / 2, y: GameEnv.innerHeight - this.size };
         this.velocity = { x: 0, y: 0 };
-        this.width = 50;
-        this.height = 50;
+        this.width = this.size;
+        this.height = this.size;
+        this.keys = {
+            right: { pressed: false },
+            left: { pressed: false }
+        };
+
+        // Bind event listeners
+        this.bindEventListeners();
     }
 
     draw(ctx) {
@@ -40,6 +50,58 @@ class Player {
         if (this.position.x < 0) {
             this.position.x = 0;
             this.velocity.x = 0;
+        }
+
+        // Handle horizontal movement
+        if (this.keys.right.pressed && this.position.x + this.width <= GameEnv.innerWidth - this.size) {
+            this.velocity.x = this.xVelocity;
+        } else if (this.keys.left.pressed && this.position.x >= this.size) {
+            this.velocity.x = -this.xVelocity;
+        } else {
+            this.velocity.x = 0;
+        }
+    }
+
+    /* These event listeners are used to handle player movement
+        * The key events perform callbacks to handle player movement
+        * The .bind(this) method ensures it refers to the Player class
+    */
+    bindEventListeners() {
+        addEventListener('keydown', this.handleKeyDown.bind(this));
+        addEventListener('keyup', this.handleKeyUp.bind(this));
+    }
+
+    handleKeyDown({ keyCode }) {
+        switch (keyCode) {
+            case 87: // 'W' key
+                this.velocity.y -= this.yVelocity;
+                break;
+            case 65: // 'A' key
+                this.keys.left.pressed = true;
+                break;
+            case 83: // 'S' key
+                this.velocity.y += this.yVelocity;
+                break;
+            case 68: // 'D' key
+                this.keys.right.pressed = true;
+                break;
+        }
+    }
+
+    handleKeyUp({ keyCode }) {
+        switch (keyCode) {
+            case 87: // 'W' key
+                this.velocity.y = 0;
+                break;
+            case 65: // 'A' key
+                this.keys.left.pressed = false;
+                break;
+            case 83: // 'S' key
+                this.velocity.y = 0;
+                break;
+            case 68: // 'D' key
+                this.keys.right.pressed = false;
+                break;
         }
     }
 }
